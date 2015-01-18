0c02a2fa3bc7fac47d13a9195deb5cb6427acbb5
hmmelton <hmmelton@uw.edu>
2015-01-17 20:12:58 -0800

    finished concurrency and error handling

--- src/main/java/hmmelton/com/stormy/MainActivity.java
+++ src/main/java/hmmelton/com/stormy/MainActivity.java
@@ -1,10 +1,15 @@
 package hmmelton.com.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -32,16 +37,49 @@ public class MainActivity extends ActionBarActivity {
                 .url(forecastUrl)
                 .build();
 
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught!", e);
-        }
+        if (isNetworkAvailable()) {
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught!", e);
+                    }
+
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, getString(R.string.network_unavailable_message),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
