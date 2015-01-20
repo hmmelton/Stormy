447d294d2dec94f3e3b45165dcb6d5a9814980e5
hmmelton <hmmelton@uw.edu>
2015-01-20 00:27:57 -0800

    finished project: populate views with actual data

--- src/main/java/hmmelton/com/stormy/CurrentWeather.java
+++ src/main/java/hmmelton/com/stormy/CurrentWeather.java
@@ -88,7 +88,7 @@ public class CurrentWeather {
     }
 
     public double getTemperature() {
-        return mTemperature;
+        return (int) Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -103,8 +103,9 @@ public class CurrentWeather {
         mHumidity = humidity;
     }
 
-    public double getPrecipChance() {
-        return mPrecipChance;
+    public int getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return (int) Math.round(precipPercentage);
     }
 
     public void setPrecipChance(double precipChance) {
